2a92c8a9f84bcffd1d9edb0d939fd2cc36568777
Anthematics <Jasonharder2@gmail.com>
2018-07-14 01:48:52 -0400

    now selects search by input value

--- scraper.js
+++ scraper.js
@@ -4,8 +4,8 @@ const nightmare = Nightmare({ show: true })
 nightmare
   .goto('https://www.google.com/')
   .type('#lst-ib', 'datatables')
+  .click('input[value= "Google Search"]')
   .then(console.log)
-
   .catch(error =>{
     console.error('fail')
   })

bdc847985d407d8e1bd56e7c506fc6e77d63244a
Anthematics <Jasonharder2@gmail.com>
2018-07-14 01:58:53 -0400

    gets into datatable website

--- scraper.js
+++ scraper.js
@@ -5,6 +5,7 @@ nightmare
   .goto('https://www.google.com/')
   .type('#lst-ib', 'datatables')
   .click('input[value= "Google Search"]')
+  .click('.rc >.r >a')
   .then(console.log)
   .catch(error =>{
     console.error('fail')

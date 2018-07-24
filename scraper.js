15fe4f39d4cc7994f235cd9cc7d452afe3c83f58
Anthematics <Jasonharder2@gmail.comX>
2018-07-24 17:13:26 -0400

    its in the finished state (well formatted but missing salary data)

--- scraper.js
+++ scraper.js
@@ -1,37 +1,33 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: false  })
+const nightmare = Nightmare({ show: false , executionTimeout: 100000})
 const fs = require('fs');
 
 
 nightmare
-  .goto('https://www.google.com/')
-  .type('#lst-ib', 'datatables')
-  .click('input[value= "Google Search"]')
-  .click('.rc >.r > a')
-  .select('select[name="example_length"]',"100")
+.goto('https://www.google.com')
+.type('#lst-ib', 'datatables')
+.click('input[value= "Google Search"]')
+.click('.rc >.r > a')
+.select('select[name="example_length"]', "100")
 
+.evaluate(function () {
+const headerFields = document.querySelectorAll("#example thead tr th")
+const peopleList = document.querySelectorAll("#example tbody tr");
+const salaryList = document.querySelectorAll(".dt-body-right")
 
-  .evaluate(function() {
-    const headerFields = document.querySelectorAll("#example thead tr th")
-    const peopleList = document.querySelectorAll("#example tbody tr");
-    const people = Array.from(peopleList).map(entry => entry.innerText.replace(/\t/g, ','))
-    const header = [...headerFields].map(headerEntry => headerEntry.innerText)
+const people = Array.from(peopleList).map(entry => entry.innerText.replace(/\t/g, ',')).join('\n')
+const header = Array.from(headerFields).map(headerEntry => headerEntry.innerText)
 
-    return {
-      log: header,
-      list: people
-    }
-  })
+return [header, people].join('\n')
+})
 
-  .end()
+.end()
 
-  .then(result => fs.writeFile('testfile.csv',JSON.stringify(result),'utf8', function(err) {
-    if (err) {
-      console.log('File not saved or corrupt');
-    } else {
-      console.log('your file is saved')
-    }
-  }))
-  .catch(error =>{
-    console.error('fail')
-  })
+.then(result => fs.writeFile('./Resultfile.csv', result, 'utf8', function (err) {
+if (err) throw err;
+console.log('your file is saved as Resultfile.csv')
+}))
+.catch((err) => {
+console.error(err);
+});
+

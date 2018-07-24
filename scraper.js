8be8b0df72ad9f51eeaaf214019c4cbfbc97fe65
Anthematics <Jasonharder2@gmail.com>
2018-07-24 00:14:09 -0400

    switched headerfields from a spread operator to array.from for easier readability

--- scraper.js
+++ scraper.js
@@ -1,5 +1,5 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: false })
+const nightmare = Nightmare({ show: false , executionTimeout: 100000})
 const fs = require('fs');
 
 
@@ -13,18 +13,19 @@ nightmare
 .evaluate(function () {
 const headerFields = document.querySelectorAll("#example thead tr th")
 const peopleList = document.querySelectorAll("#example tbody tr");
+const salaryList = document.querySelectorAll(".dt-body-right")
 
 const people = Array.from(peopleList).map(entry => entry.innerText.replace(/\t/g, ',')).join('\n')
-const header = [...headerFields].map(headerEntry => headerEntry.innerText)
+const header = Array.from(headerFields).map(headerEntry => headerEntry.innerText)
 
 return [header, people].join('\n')
 })
 
 .end()
 
-.then(result => fs.writeFile('./testfile.csv', result, 'utf8', function (err) {
+.then(result => fs.writeFile('./Resultfile.csv', result, 'utf8', function (err) {
 if (err) throw err;
-console.log('your file is saved')
+console.log('your file is saved as Resultfile.csv')
 }))
 .catch((err) => {
 console.error(err);
